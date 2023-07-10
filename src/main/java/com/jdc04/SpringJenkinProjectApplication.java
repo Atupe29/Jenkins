1eeac31a9d2b6eafb9cc4751267ee81e5f5a768f
Arjun <atupe439@gmail.com>
2023-07-10 19:34:17 +0530

    uplode

---
+++ src/main/java/com/jdc04/SpringJenkinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jdc04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinProjectApplication.class, args);
+	}
+
+}
